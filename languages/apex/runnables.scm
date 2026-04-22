ceac7572d226cb19747cfea37fe5a50cfa284a24
Adam Stepanek <stepanek.ada@seznam.cz>
2026-04-22 22:10:58 +0200

    Add Apex test execution tasks
    Introduces tasks for running Apex tests at both the class and method
    level using the `sf` CLI. This builds on the previously added
    Tree-sitter grammar for identifying test classes and methods.

--- languages/apex/runnables.scm
+++ languages/apex/runnables.scm
@@ -1,3 +1,15 @@
+; Class-level: @isTest annotation on class
+(
+  (class_declaration
+    (modifiers
+      (annotation
+        name: (identifier) @_ann
+        (#match? @_ann "(?i)^isTest$")))
+    name: (identifier) @run)
+  (#set! tag apex-test-class)
+)
+
+; Method-level: testMethod modifier
 (
   (class_declaration
     (class_body
@@ -9,6 +21,7 @@
   (#set! tag apex-test)
 )
 
+; Method-level: @isTest annotation on method
 (
   (class_declaration
     (class_body
